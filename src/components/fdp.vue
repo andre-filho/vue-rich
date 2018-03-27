54debb363d4403e6450e5fa85bd6f40d8e57bb1e
Andre de Sousa Costa Filho <andre.filho001@outlook.com>
2018-03-27 09:08:29 -0300

    component creation

---
+++ src/components/fdp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    dksjfkjasklfjskljfksldjfklsd
+    {{ nome_cu }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nome_cu: 'rich'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Fue from '../components/fdp'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/cu',
+      name: 'FDP',
+      component: Fue
     }
   ]
 })
